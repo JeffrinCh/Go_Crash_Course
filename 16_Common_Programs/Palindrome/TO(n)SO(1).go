3f7709aec5b89d5a79be2eb435f641590b5418f1
Jeff <cjeffrin@gmail.com>
2020-09-18 00:43:40 +0530

    Palindrome: time: O(n) space: O(1)

--- 16_Common_Programs/Palindrome/TO(n)SO(1).go
+++ 16_Common_Programs/Palindrome/TO(n)SO(1).go
@@ -1,12 +1,12 @@
 package main
+
 //import "fmt"
 
 // Time Complexity O(n) Space o(n)
 func IsPalindrome(str string) bool {
 	length := len(str)
 	for i, r := range str {
-		
-		if int(r) != int(str[length - i - 1]) {
+		if int(r) != int(str[length-i-1]) {
 			return false
 		}
 	}
